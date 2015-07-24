988e0b4ac44ade5a9688f6bf11537d467705304e
Daniel Noland <daniel.noland@gmail.com>
2015-07-23 23:27:05 -0600

    Fix copy of setup.py

--- setup.py
+++ setup.py
@@ -13,12 +13,11 @@ requires = [
     'pyramid_chameleon',
     'pyramid_debugtoolbar',
     'waitress',
-    'tornado'
     ]
 
-setup(name='memoryoracle_server',
+setup(name='memoryoracle_webui',
       version='0.0',
-      description='memoryoracle_server',
+      description='memoryoracle_webui',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
         "Programming Language :: Python",
@@ -26,18 +25,18 @@ setup(name='memoryoracle_server',
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
         ],
-      author='',
-      author_email='',
-      url='',
-      keywords='web pyramid pylons',
+      author='Daniel Austin Noland',
+      author_email='daniel.noland@gmail.com',
+      url='https://github.com/MemoryOracle/memoryoracle_webui',
+      keywords='web pyramid pylons gdb debug',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
       install_requires=requires,
       tests_require=requires,
-      test_suite="memoryoracle_server",
+      test_suite="memoryoracle_webui",
       entry_points="""\
       [paste.app_factory]
-      main = memoryoracle_server:main
+      main = memoryoracle_webui:main
       """,
       )
