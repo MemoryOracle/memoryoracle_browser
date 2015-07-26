7fa4789672e5364dcb337c2899951e8658ca8384
Daniel Noland <daniel.noland@gmail.com>
2015-07-26 11:31:22 -0600

    Restructured build process.
    Working toward a plugable webui.

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,43 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var traceur = require('gulp-traceur');
-var bob = require('bob');
-// var yaml = require('js-yaml');
-// var fs = require('fs');
 var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
 
-// var uglify = require('gulp-uglify');
-// var stream = require('stream-combiner2');
+var GLOBAL_SOURCE = "memoryoracle_webui/www";
+var GLOBAL_DEST = "build/www";
 
-var mapper = bob();
 gulp.task('javascript', function() {
-   mapper.src('js')
+   var RELPATH = ["/js/", "**/*.js"];
+   var SOURCE = GLOBAL_SOURCE + RELPATH[0] + RELPATH[1];
+   var DEST = GLOBAL_DEST + RELPATH[0];
+   return gulp.src([traceur.RUNTIME_PATH, SOURCE])
       .pipe(sourcemaps.init())
       .pipe(traceur())
+      .pipe(uglify())
       .pipe(sourcemaps.write())
-      .pipe(mapper.dest('js'));
+      .pipe(gulp.dest(DEST));
 });
 
 gulp.task('html', function() {
-   mapper.src('html')
+   var RELPATH = ["/html/", "**/*.html"];
+   var SOURCE = GLOBAL_SOURCE + RELPATH[0] + RELPATH[1];
+   var DEST = GLOBAL_DEST + RELPATH[0];
+   return gulp.src(SOURCE)
       .pipe(sourcemaps.init())
       .pipe(sourcemaps.write())
-      .pipe(mapper.dest('html'));
+      .pipe(gulp.dest(DEST));
 });
 
-gulp.task('default', ['javascript', 'html']);
+gulp.task("libraries", function() {
+   var RELPATH = ["/lib/", "**/*"];
+   var SOURCE = GLOBAL_SOURCE + RELPATH[0] + RELPATH[1];
+   var DEST = GLOBAL_DEST + RELPATH[0];
+   return gulp.src(SOURCE)
+      .pipe(sourcemaps.init())
+      .pipe(uglify())
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest(DEST));
+})
+
+gulp.task('default', ['javascript', 'html', 'libraries']);
